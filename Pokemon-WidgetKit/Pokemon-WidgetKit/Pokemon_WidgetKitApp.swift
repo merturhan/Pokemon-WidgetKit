61ffb430d276fa5879313cda7c29cd69f9b30231
Mert Urhan <mert-urhan@hotmail.com>
2023-01-31 15:11:34 +0300

    First Commit

---
+++ Pokemon-WidgetKit/Pokemon-WidgetKit/Pokemon_WidgetKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pokemon_WidgetKitApp.swift
+//  Pokemon-WidgetKit
+//
+//  Created by Mert Urhan on 31.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct Pokemon_WidgetKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
