95f396e3c5613110285c23ecdecd5d03ed153c70
Mert Urhan <mert-urhan@hotmail.com>
2023-01-31 15:20:12 +0300

    Pokemon struct defined

---
+++ Pokemon-WidgetKit/Pokemon-WidgetKit/Pokemon.swift
@@ -0,0 +1,24 @@
+//
+//  Pokemon.swift
+//  Pokemon-WidgetKit
+//
+//  Created by Mert Urhan on 31.01.2023.
+//
+
+import Foundation
+
+struct Pokemon : Identifiable, Codable {
+    
+    let picName : String
+    let name : String
+    let type : String
+    
+    var id : String{
+        picName
+    }
+}
+
+let pikachu = Pokemon(picName: "pikachu", name: "Pikachu", type: "Mouse Pokemon")
+let charmander = Pokemon(picName: "charmander", name: "Charmander", type: "Lizard Pokemon")
+let clefairy = Pokemon(picName: "clefairy", name: "Clefairy", type: "Fairy Pokemon")
+
